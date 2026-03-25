369e29ef9fd7c1ea2a0ed4d1d21a9e4d067b7fbc
Ubuntu <ubuntu@ip-172-26-7-180.ap-southeast-1.compute.internal>
2026-03-25 03:53:52 +0000

    新增 require 示例脚本

---
+++ test5.lua
@@ -0,0 +1,6 @@
+local test6 = require("test6")
+
+local result = test6.add(20, 22)
+print("test6.add result:", result)
+
+return result
---
+++ test6.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.add(a, b)
+  return a + b
+end
+
+return M
