a0688bfcc440427aaad3a87452a82f9775e9ade3
lifan171 <lifan171@126.com>
2026-03-25 16:34:30 +0800

    test6.lua

--- test6.lua
+++ test6.lua
@@ -1,6 +1,26 @@
 local M = {}
 
 function M.add(a, b)
+
+  local iterations = 10
+  local sum = 0
+  
+  local start_ms = clock_ms()
+  local nt = {}
+  for i = 1, iterations do
+      nt[i] = i
+      sum = sum + i
+      print("iteration", i, "sum", nt[i])
+  end
+  
+  local finish_ms = clock_ms()
+  local elapsed_ms = finish_ms - start_ms
+  print("start_ms", start_ms)
+  print("finish_ms", finish_ms)
+  print("iterations", iterations)
+  print("sum", sum)
+  print("elapsed_ms", elapsed_ms)
+  print(math.max(-111, 1))
   return a + b
 end
 
